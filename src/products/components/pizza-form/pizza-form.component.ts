9e81ee710f7bb24643d096402339b18e8d35d6d5
Suz Oliver <oliver.suzanne2@mayo.edu>
2018-09-06 15:01:33 -0500

    route guards added

--- src/products/components/pizza-form/pizza-form.component.ts
+++ src/products/components/pizza-form/pizza-form.component.ts
@@ -5,14 +5,14 @@ import {
   EventEmitter,
   OnChanges,
   SimpleChanges,
-  ChangeDetectionStrategy,
+  ChangeDetectionStrategy
 } from '@angular/core';
 import {
   FormControl,
   FormGroup,
   FormArray,
   FormBuilder,
-  Validators,
+  Validators
 } from '@angular/forms';
 
 import { map } from 'rxjs/operators';
@@ -22,15 +22,16 @@ import { Topping } from '../../models/topping.model';
 
 @Component({
   selector: 'pizza-form',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['pizza-form.component.scss'],
   template: `
     <div class="pizza-form">
       <form [formGroup]="form">
-      
+
         <label>
           <h4>Pizza name</h4>
-          <input 
-            type="text" 
+          <input
+            type="text"
             formControlName="name"
             placeholder="e.g. Pepperoni"
             class="pizza-form__input"
@@ -41,7 +42,7 @@ import { Topping } from '../../models/topping.model';
             <p>Pizza must have a name</p>
           </div>
         </label>
-      
+
         <ng-content></ng-content>
 
         <label>
@@ -84,22 +85,28 @@ import { Topping } from '../../models/topping.model';
 
       </form>
     </div>
-  `,
+  `
 })
 export class PizzaFormComponent implements OnChanges {
   exists = false;
 
-  @Input() pizza: Pizza;
-  @Input() toppings: Topping[];
+  @Input()
+  pizza: Pizza;
+  @Input()
+  toppings: Topping[];
 
-  @Output() selected = new EventEmitter<number[]>();
-  @Output() create = new EventEmitter<Pizza>();
-  @Output() update = new EventEmitter<Pizza>();
-  @Output() remove = new EventEmitter<Pizza>();
+  @Output()
+  selected = new EventEmitter<number[]>();
+  @Output()
+  create = new EventEmitter<Pizza>();
+  @Output()
+  update = new EventEmitter<Pizza>();
+  @Output()
+  remove = new EventEmitter<Pizza>();
 
   form = this.fb.group({
     name: ['', Validators.required],
-    toppings: [[]],
+    toppings: [[]]
   });
 
   constructor(private fb: FormBuilder) {}
